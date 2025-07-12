41518ecdc967ce0329fd7b54f18db16c315ff0a4
harshdeveloper0 <piyushsingh1740x@gmail.com>
2025-07-13 02:38:45 +0530

    second

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["cdn.mos.cms.futurecdn.net","tse1.mm.bing.net", "tse2.mm.bing.net", "tse3.mm.bing.net",'hemophilianewstoday.com','www.quanticate.com'],
+  },
+};
+
+export default nextConfig;
